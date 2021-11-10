ca75b0aee2945ae6fdb43fd27c9808c6fbdc256e
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2021-11-10 14:33:56 +0100

    Fixing code by phpstan rules

--- app/Policies/RoomPolicy.php
+++ app/Policies/RoomPolicy.php
@@ -12,29 +12,6 @@ class RoomPolicy
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Room  $room
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function view(User $user, Room $room)
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can create models.
      *
      * @return bool
@@ -46,10 +23,6 @@ class RoomPolicy
 
     /**
      * Check if a user is able to invite into a room.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Room  $room
-     * @return bool
      */
     public function invite(User $user, Room $room): bool
     {
@@ -58,50 +31,10 @@ class RoomPolicy
 
     /**
      * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Room  $room
-     * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function update(User $user, Room $room)
+    public function update(User $user, Room $room): bool
     {
-        return $room->users()->wherePivotIn('role', [RoomRoleEnum::OWNER(), RoomRoleEnum::MANAGER()])->get()
-            ->contains($user);
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Room  $room
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, Room $room)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Room  $room
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function restore(User $user, Room $room)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Room  $room
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function forceDelete(User $user, Room $room)
-    {
-        //
+        return $room->users()->wherePivotIn('role', [RoomRoleEnum::OWNER(), RoomRoleEnum::MANAGER()])
+            ->where('user_id', $user->id)->exists();
     }
 }
