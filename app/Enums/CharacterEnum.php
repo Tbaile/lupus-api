fac53ad07330a7afc9baf9a4aded06187d75a9c9
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2022-04-12 16:36:54 +0200

    Added characters enum

---
+++ app/Enums/CharacterEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Enums;
+
+use Spatie\Enum\Laravel\Enum;
+
+/**
+ * @method static self WOLF()
+ * @method static self VILLAGER()
+ * @method static self FAIRY()
+ */
+final class CharacterEnum extends Enum
+{
+}
