e905797e1f2d244ed3b778b26386c68e33ab58c3
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2022-06-10 17:07:18 +0200

    Removed handle call, added more cases

--- tests/Unit/EngineTest.php
+++ tests/Unit/EngineTest.php
@@ -1,22 +1,29 @@
 <?php
 
+use App\Engine\EngineData;
 use App\Engine\EngineFactory;
 use App\Engine\FairyVote;
 use App\Engine\WolfVote;
 use App\Exceptions\NotImplemented;
 use App\Models\Game;
+use Illuminate\Http\Request;
 
 use function PHPUnit\Framework\assertTrue;
 
 test('can make engine', function (array $chain) {
     $handler = (new EngineFactory($chain))->make();
-    assertTrue($handler instanceof WolfVote);
-    $handler->handle(new \Illuminate\Http\Request(), new Game());
+    assertTrue($handler instanceof $chain[0]);
 })->with([
     [
         [
             WolfVote::class,
             FairyVote::class
+        ],
+        [
+            FairyVote::class
+        ],
+        [
+            WolfVote::class
         ]
     ]
-])->throws(NotImplemented::class);
+]);
