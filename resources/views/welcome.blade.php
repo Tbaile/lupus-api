c5b8841e11e7a42fe0a71bfdcd50bf20da3c9231
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2022-06-30 10:54:31 +0200

    Replaced laravel mix with vite

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite(['resources/css/app.css'])
     <title>{{ config('app.name') }}</title>
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}"/>
 </head>
 <body class="h-screen flex bg-black">
 <img alt="Sax Gandalf" class="object-contain w-full" src="img/sax_gandalf.gif">
-<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
