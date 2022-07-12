7d3da2e7a74573a319ddd4ccd9b87b67db513d13
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2022-07-12 16:31:42 +0200

    Compared to last laravel installation

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,13 +13,13 @@ class UserFactory extends Factory
     /**
      * Define the model's default state.
      *
-     * @return array
+     * @return array<string, mixed>
      */
     public function definition(): array
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => fake()->name(),
+            'email' => fake()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
@@ -29,11 +29,11 @@ class UserFactory extends Factory
     /**
      * Indicate that the model's email address should be unverified.
      *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     * @return static
      */
     public function unverified(): Factory
     {
-        return $this->state(function () {
+        return $this->state(function (array $attributes) {
             return [
                 'email_verified_at' => null,
             ];
