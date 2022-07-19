f3d72e0affa17fe03935a11278fa68d25733333b
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2022-07-19 17:01:01 +0200

    Moved FairyTest in Unit Testing

--- tests/Unit/FairyTest.php
+++ tests/Unit/FairyTest.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Engine\Checks\Exceptions\CharacterShouldBeAliveException;
+use App\Engine\Checks\Exceptions\UserAlreadyVotedExeption;
 use App\Engine\EngineData;
 use App\Engine\Votes\FairyVote;
 use App\Enums\CharacterEnum;
+use Illuminate\Support\Collection;
 
 uses()->group('game', 'engine', 'fairy');
 
@@ -16,3 +19,22 @@ test('null on every other character', function ($character) {
 })->with(
     fn() => collect(CharacterEnum::cases())->filter(fn(CharacterEnum $value) => !$value->equals(CharacterEnum::FAIRY()))
 );
+
+test('cannot vote if dead', function () {
+    $fairyHandler = new FairyVote(null);
+    $fakeData = mock(EngineData::class)->expect(
+        getCharacter: fn() => CharacterEnum::FAIRY(),
+        isAlive: fn() => false
+    );
+    expect($fairyHandler->handle($fakeData));
+})->throws(CharacterShouldBeAliveException::class);
+
+test('cannot vote if already voted', function () {
+    $fairyHandler = new FairyVote(null);
+    $fakeData = mock(EngineData::class)->expect(
+        getCharacter: fn() => CharacterEnum::FAIRY(),
+        isAlive: fn() => true,
+        getActions: fn() => new Collection([1])
+    );
+    expect($fairyHandler->handle($fakeData));
+})->throws(UserAlreadyVotedExeption::class);
